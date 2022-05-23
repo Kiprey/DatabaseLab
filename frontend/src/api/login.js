4f8f0077c4df3690018e781561303f7eb831277e
Kiprey <Kiprey@qq.com>
2022-05-23 16:49:26 +0800

    save login logic markdown

--- frontend/src/api/login.js
+++ frontend/src/api/login.js
@@ -4,5 +4,8 @@ export default {
   login: query => postWithLoadTip(`/api/account/login`, query),
   logout: () => post(`/api/account/logout`),
   register: query => post(`/api/account/register`, query),
-  changePass: query => post(`/api/account/changepass`, query)
+  changePass: query => post(`/api/account/changepass`, query),
+
+  getInfo: () => post('/api/account/getinfo'),
+  saveInfo: () => post('/api/account/saveinfo')
 }
